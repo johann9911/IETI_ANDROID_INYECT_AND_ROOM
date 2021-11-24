79632b50f801048690d09a501426a45902720f20
Johann Bogota <johann.bogota@mail.escuelaing.edu.co>
2021-11-23 20:22:41 -0500

    add class aplication

---
+++ app/src/main/java/com/example/android_task_planner/HiltApplication.java
@@ -0,0 +1,9 @@
+package com.example.android_task_planner;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class HiltApplication extends Application {
+}
